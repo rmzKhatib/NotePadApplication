476a398fa8be2d3cc49fcbc8ebd84d0c78170c18
rmzKhatib <ramzikhatib07@gmail.com>
2024-07-18 23:00:14 -0400

    Added Scroll Bar
    Added vertical and horizontal scroll bar

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,13 @@
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class GUI {
 
     JFrame window;
+    JTextArea textArea;
+    JScrollPane scrollPane;
     public static void main(String[] args) {
 
         new GUI();
@@ -11,14 +16,23 @@ public class GUI {
     public GUI() {
 
         createWindow();
+        createTextArea();
 
         window.setVisible(true);
 
     }
     public void createWindow() {
-        window = new JFrame("NotePad");
-        window.setSize(800, 600);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window = new JFrame("NotePad"); // window title
+        window.setSize(800, 600); // window size
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allows you to close the window
 
+    }
+    public void createTextArea() {
+
+        textArea = new JTextArea();
+
+        scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);// Initialize Scroll Bar
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        window.add(scrollPane); // Adds scroll bar
     }
 }
